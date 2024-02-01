df428613eb5bb2535edff382699c64cf69ef02bc
Digmoss <158097103+Digmoss@users.noreply.github.com>
2024-02-01 22:01:28 +0200

    Update Avi_Digmi.java

--- Avi_Digmi.java
+++ Avi_Digmi.java
@@ -1,7 +1,7 @@
 public class Avi_Digmi{  
 	public static void main(String[] args) {   
 		for (int i=1;i<=10; i++) {    
-			System.out.println("Avi Digmi");
+			System.out.println("Believe in yourself");
 		}  
 	} 
 } 
